31a6a0c21f7a79566bdbf3328a84dc5bbff8edbd
Nicholas de Waal <nick.a.dewaal@gmail.com>
2020-12-07 20:18:26 +0900

    first react commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -8,15 +8,15 @@ app.use(express.json({ extended: false }));
 // connect database
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('API RUNNING');
+app.get("/", (req, res) => {
+  res.send("API RUNNING");
 });
 
 // Define Routers
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
